2927c6e087be86c5de200e9c6d4fe6f18b17c2e2
ShTair <git@vsh.jp>
2019-10-15 13:15:56 +0900

    コンボリューションチェック

--- ConvolutionChecker/Program.cs
+++ ConvolutionChecker/Program.cs
@@ -1,8 +1,6 @@
-﻿using System;
+﻿using KelpNet;
+using KelpNet.CL;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ConvolutionChecker
 {
@@ -10,6 +8,26 @@ namespace ConvolutionChecker
     {
         static void Main(string[] args)
         {
+            RunAsync();
+        }
+
+        private static void RunAsync()
+        {
+            var trainData = new NdArray(new[] { 2, 3, 4 });
+            for (int i = 0; i < trainData.Data.Length; i++)
+            {
+                trainData.Data[i] = (float)i / trainData.Data.Length;
+            }
+
+            var functions = new List<Function>();
+            functions.Add(new Convolution2D(2, 1, 3));
+
+            var nn = new FunctionStack(functions.ToArray());
+            nn.Compress();
+            var optimizer = new Adam();
+            nn.SetOptimizer(optimizer);
+
+            var result = nn.Predict(trainData)[0];
         }
     }
 }
